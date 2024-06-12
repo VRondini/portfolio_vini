86fcb8884d2fd75d5f816395498560e54544eeb9
Vinicius Rondini <rondini405@gmail.com>
2024-06-12 00:24:54 -0300

    Adicionadas mais traduções

--- assets/js/languages.js
+++ assets/js/languages.js
@@ -7,6 +7,8 @@ const texts = {
         dropdownLanguages: "Idiomas",
         switchLanguageButton: "Alterar Língua",
         presentationGreeting: "Olá,",
+        presentationName: "Eu sou o Vinicius Rondini!",
+        aboutMeText: "Eu sou brasileiro, nascido em Campinas, São Paulo. Como criança, assim como qualquer outra, eu queria entender como tudo funcionava, especialmente a internet e seus belos websites. Hoje, não só continuo curioso, como fico muito feliz de poder desenvolver esses websites."
     },
     en: {
         navHomeButton: "Home",
@@ -16,6 +18,8 @@ const texts = {
         dropdownLanguages: "Languages",
         switchLanguageButton: "Change Language",
         presentationGreeting: "Hi,",
+        presentationName: "I'm Vinicius Rondini!",
+        aboutMeText: "I'm Brazilian, native of Campinas, São Paulo. As a child, like any other kid, I wanted to understand how everything worked, especially the internet and its beautiful websites. Today, not only am I still curious, but I'm amazed that I can develop those websites."
     }
 };
 
@@ -29,5 +33,7 @@ function switchLanguage() {
     document.getElementById('nav-projects-button').textContent = texts[currentLang].navProjectsButton;
     document.getElementById('nav-languages-button').textContent = texts[currentLang].dropdownLanguages;
     document.getElementById('language-button-id').textContent = texts[currentLang].switchLanguageButton;
-    document.getElementById('presentation-name').textContent = texts[currentLang].presentationGreeting;
+    document.getElementById('presentation-greeting').textContent = texts[currentLang].presentationGreeting;
+    document.getElementById('presentation-name').textContent = texts[currentLang].presentationName;
+    document.getElementById('about-me-text-id').textContent = texts[currentLang].aboutMeText;
 }
